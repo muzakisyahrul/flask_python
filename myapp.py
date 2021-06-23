28502a0c4a2bad0dcea575a1afe7c1353e36f34b
unknown <muzakisyahrul100@gmail.com>
2021-06-23 22:50:33 +0700

    Flask 2

--- myapp.py
+++ myapp.py
@@ -1,9 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template;
+
 app = Flask(__name__)
 
 @app.route("/index")
 def home():
-    return "Hallo ini Flask"
+	# Looping
+	hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']
+	# Conditional
+	ujian = "tidak lulus" # Jika lulus saya akan senang, jika tidak lulus saya sedih
+	return render_template("index.html", hari=hari, ujian=ujian)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+	if __name__ == "__main__":
+		app.run(debug=True)
